b93de4f461cb236161a7203a9c5e16343b406501
sophiafayer <sophia.k.fayer@vanderbilt.edu>
2024-02-14 14:08:50 -0600

    Initial Commit

---
+++ ecoVandy/ecoVandyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ecoVandyApp.swift
+//  ecoVandy
+//
+//  Created by Sophia Fayer on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct ecoVandyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
